a595be52b7edc5fbed20356d1d8004e2eb85bbfc
Jennifer Fahey <jenguin777@gmail.com>
2018-08-11 14:52:11 -0400

    fixed switch / case statements and method call wrappers, added handling when no movie passed, use Mr. Nobody in OMDB

--- liri.js
+++ liri.js
@@ -55,7 +55,16 @@ var client = new Twitter({
 
 // Wrapping the client.get call in a function or case statement does NOT work...I get TypeError: Cannot read property 'get' of undefined
 function myTweets() {
-// Call client.get method from twitter npm pkg
+
+    // Create instance of Twitter object, actual keys not displayed this way
+    var client = new Twitter({
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token_key: keys.twitter.access_token_key,
+    access_token_secret: keys.twitter.access_token_secret,
+  });
+    
+  // Call client.get method from twitter npm pkg
     var params = {screen_name: 'jenguin777', count: 20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
@@ -73,34 +82,43 @@ function myTweets() {
 
 //----------------PROCESS SPOTIFY-----------------------------//
 
-var spotify = new Spotify({
-    id: keys.spotify.id,
-    secret: keys.spotify.secret
-});
-
 // Wrapping the spotify.search call in a function or case statement does NOT work...I get TypeError: Cannot read property 'get' of undefined
 function spotifySong() {
 
-    var songName = "";
-    nodeArgs = process.argv;
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+
+    console.log("spotify object" + JSON.stringify(spotify, null, 4));
+    var nodeArgs = process.argv.slice(3).join(' ');
+    console.log("nodeArgs: " + nodeArgs);
+
+    // Create an empty variable for holding the movie name
+    var songName = nodeArgs; 
+
+
+    // var songName = "";
+    // nodeArgs = process.argv;
 
     // Loop through all the words in the node argument starting at index 3
-    for (var i = 3; i< nodeArgs.length; i++) {
-        //strip off the last plus sign
-        if(i > 3 && i < nodeArgs.length) {
-            songName = songName + "+" + nodeArgs[i];
-        }
-        else {
-            songName += nodeArgs[i];
-        }
-    }
+    // for (var i = 3; i< nodeArgs.length; i++) {
+    //     //strip off the last plus sign
+    //     if(i > 3 && i < nodeArgs.length) {
+    //         songName = songName + "+" + nodeArgs[i];
+    //     }
+    //     else {
+    //         songName += nodeArgs[i];
+    //     }
+    // }
+
     console.log("songName: " + songName);
 
 
     // added limit 1 so it will return only 1 song
     spotify.search({ type: 'track', query: songName, limit: 1}, function(error, song) {
         // If a song is entered, fetch details 
-        if (songName) {
+        if (song) {
             if (!error) {
                 // console.log(JSON.stringify(song, null, 2));
                 console.log("|REMOVE BEFORE SUBMISSION--------BEGIN SPOTIFY---------|");
@@ -109,7 +127,7 @@ function spotifySong() {
                 // Return the artist's name
                 console.log("Artist's name: " + song.tracks.items[0].artists[0].name);
                 // Return the album name
-                console.log(song.tracks.items[0].album.name);
+                console.log("Album name: " + song.tracks.items[0].album.name);
                 if (song.tracks.items[0].preview_url) {
                     // If present, return the preview link
                     console.log("Preview URL: " + song.tracks.items[0].preview_url);
@@ -134,49 +152,74 @@ function spotifySong() {
 function movieThis() {
     
     console.log("|REMOVE BEFORE SUBMISSION--------BEGIN OMDB------------|");
-    // Store all of the arguments in an array
-    var nodeArgs = process.argv;
+    // Store all of the arguments in an array -- problem with this is that it doesn't handle when no movie was passed
+    var nodeArgs = process.argv.slice(3).join(' ');
+    console.log("nodeArgs: " + nodeArgs);
 
     // Create an empty variable for holding the movie name
-    var movieName = "";
+    var movieName = nodeArgs; //this may be evaluating to false....need to check
+    // --------remove this???  if(movieName) {
+        // // Loop through all the words in the node argument
+        // // And do a little for-loop magic to handle the inclusion of "+"s
+        //     for (var i = 3; i < nodeArgs.length; i++) {
+        //         console.log("movieName: " + movieName);
+        //         if (i > 3 && i < nodeArgs.length) {
+        //             movieName = movieName + "+" + nodeArgs[i];
+        //         }
+        //         else {
+        //             movieName += nodeArgs[i];
+        //         }
+        //     }
+        console.log("movieName: " + movieName);
 
-        if(movieName) {
-        // Loop through all the words in the node argument
-        // And do a little for-loop magic to handle the inclusion of "+"s
-            for (var i = 3; i < nodeArgs.length; i++) {
-                console.log("movieName: " + movieName);
-                if (i > 3 && i < nodeArgs.length) {
-                    movieName = movieName + "+" + nodeArgs[i];
-                }
-                else {
-                    movieName += nodeArgs[i];
-                }
-            }
-
-        // Then run a request to the OMDB API with the movie specified
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-        // Submit a request to OMDB API
-        request(queryUrl, function(error, response, body) {
-
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-
-            // Parse the body of the site and recover the data we want
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country where movie was produced: " + JSON.parse(body).Country);
-            console.log("Language of the movie: " + JSON.parse(body).Language);
-            console.log("Plot of the movie: " + JSON.parse(body).Plot);
-            console.log("Actors in the movie: " + JSON.parse(body).Actors);
-        }
-        });
         // If no movie was provided, set movieName to "Mr. Nobody" and make another call to movieThis() function
+        if (movieName.length === 0) {
+            movieName = "Mr. Nobody";
+            // Then run a request to the OMDB API with the movie specified
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+            // Submit a request to OMDB API
+            request(queryUrl, function(error, response, body) {
+
+                // If the request is successful
+                if (!error && response.statusCode === 200) {
+
+                    // Parse the body of the site and recover the data we want
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                    // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country where movie was produced: " + JSON.parse(body).Country);
+                    console.log("Language of the movie: " + JSON.parse(body).Language);
+                    console.log("Plot of the movie: " + JSON.parse(body).Plot);
+                    console.log("Actors in the movie: " + JSON.parse(body).Actors);
+                    console.log(body);
+                }
+            });
+            
         } else {
-        movieName = "Mr. Nobody";
-        // movieThis();
-    }
+                // Then run a request to the OMDB API with the movie specified
+                var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+                console.log(queryUrl);
+
+                // Submit a request to OMDB API
+                request(queryUrl, function(error, response, body) {
+
+                    // If the request is successful
+                    if (!error && response.statusCode === 200) {
+                        console.log(body);
+                        // Parse the body of the site and recover the data we want
+                        // console.log("Title: " + JSON.parse(body).Title);
+                        // console.log("Release Year: " + JSON.parse(body).Year);
+                        // console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                        // // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                        // console.log("Country where movie was produced: " + JSON.parse(body).Country);
+                        // console.log("Language of the movie: " + JSON.parse(body).Language);
+                        // console.log("Plot of the movie: " + JSON.parse(body).Plot);
+                        // console.log("Actors in the movie: " + JSON.parse(body).Actors);
+                    }
+                });
+            }
+    // }
 console.log("|REMOVE BEFORE SUBMISSION--------END OMDB--------------|");
 }
